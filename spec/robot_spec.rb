684b55a09352a9ba8ec5e19b1996686e59ec6fcb
Keith Broughton <keithbro256@gmail.com>
2017-06-14 22:09:09 +0100

    Add tests for robot and world

---
+++ spec/robot_spec.rb
@@ -0,0 +1,33 @@
+require "robot"
+require "world"
+
+describe Robot do
+  world = World.new()
+  robot = Robot.new()
+
+  it "can report its status" do
+    world.place(robot, 1, 2, World::SOUTH)
+    expect(robot.report()).to eql([1, 2, World::SOUTH])
+  end
+
+  it "can move" do
+    world.place(robot, 0, 0, World::NORTH)
+    robot.move()
+    expect(robot.report()).to eql([0, 1, World::NORTH])
+  end
+
+  it "can turn left" do
+    world.place(robot, 0, 0, World::NORTH)
+    robot.left()
+    expect(robot.report()).to eql([0, 0, World::WEST])
+  end
+
+  it "can move multiple times" do
+    world.place(robot, 1, 2, World::EAST)
+    robot.move()
+    robot.move()
+    robot.left()
+    robot.move()
+    expect(robot.report()).to eql([3, 3, World::NORTH])
+  end
+end
---
+++ spec/world_spec.rb
@@ -0,0 +1,16 @@
+require "world"
+
+describe World do
+  world = World.new()
+  robot = Robot.new()
+
+  it "cannot place something off the table" do
+    expect {
+      world.place(robot, 5, 5, World::NORTH)
+    }.to raise_error(RuntimeError, "Invalid position (5, 5)")
+
+    expect {
+      world.place(robot, -1, -1, World::NORTH)
+    }.to raise_error(RuntimeError, "Invalid position (-1, -1)")
+  end
+end
