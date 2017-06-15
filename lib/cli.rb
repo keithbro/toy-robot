f42f806dd99ed56b65ac076839c04508f118b6bf
Keith Broughton <keithbro256@gmail.com>
2017-06-15 19:56:26 +0100

    Implement CLI

---
+++ lib/cli.rb
@@ -0,0 +1,22 @@
+require 'robot'
+require 'world'
+
+class CLI
+  def initialize()
+    @robot = Robot.new
+    @world = World.new
+  end
+
+  def execute(command)
+    world_method = command.split(/ /)[0].downcase
+    args = ( command.split(/ /)[1] || '' )
+      .split(/,/)
+      .map { |arg| arg.match?(/^\d+$/) ? arg.to_i : arg }
+
+    result = @world.send(world_method, @robot, *args)
+
+    return result.join(',') if world_method.eql?('report')
+
+    nil
+  end
+end
